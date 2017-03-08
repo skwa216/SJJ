868b8c1a551c8d2cb86825574050782e389174f6
skwa216 <skwa216@gmail.com>
2017-03-08 14:24:32 -0500

    Add files via upload

---
+++ SJJ-master/TheDVDExchangeWebsite/HomePage.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+//2/23/17
+//Sam Kelsey, Jacob Homanics, Jordyn DiSanzo
+//This class opens the database connection and stores into a session
+public partial class HomePage : System.Web.UI.Page
+{
+    public ErrorMessage em = new ErrorMessage();
+    //Initializes the dbConnection to the database
+    static SqlConnection dbConnection = new SqlConnection("Data Source=stusql;Initial Catalog=TheDVDExchangeDatabase ;Integrated Security=true");
+    //Initializes bool that is used to check if the user
+
+    public SQLConnections skwal = new SQLConnections();
+
+    //is currently logged in
+    public bool loggedIn = false;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        skwal.closeDatabase();
+
+        skwal.openDatabase();
+        
+        if (ErrorMessage.printMe)
+        {
+            Response.Write(em.returnMessage());
+            ErrorMessage.printMe = false; 
+        }
+
+        skwal.closeDatabase();
+       
+
+
+
+        //If statement checks to see whether or not to print a message
+
+        //Sets the Session to the value of loggedIn
+        if (Session["loginstatus"] != null)
+        {
+            loggedIn = (bool)Session["loginstatus"];
+
+        }
+        //Opens database Connection
+    //    dbConnection.Open();
+        //Sets the session to the dbConnection
+     //   Session["DatabaseConnection"] = dbConnection;
+        //Closes dbConnection
+     //   dbConnection.Close();
+
+    }
+    protected void LoadSignUpPage(object sender, EventArgs e)
+    {
+        if (loggedIn)
+        {
+            ErrorMessage.message = "You are currently logged in!";
+            ErrorMessage.printMe = true;
+            Response.Redirect("HomePage.aspx");
+        }
+        if (!loggedIn)
+        {
+            Response.Redirect("SignUpPage.aspx");
+        }
+
+    }
+    protected void LoadLoginPage(object sender, EventArgs e)
+    {
+        if (loggedIn)
+        {
+            ErrorMessage.message = "You are currently logged in!";
+            ErrorMessage.printMe = true;
+            Response.Redirect("HomePage.aspx");
+        }
+        if (!loggedIn)
+        {
+
+            Response.Redirect("LoginPage.aspx");
+
+        }
+
+    }
+    protected void LoadBrowsePage(object sender, EventArgs e)
+    {
+        if (loggedIn)
+        {
+            Response.Redirect("BrowsePage.aspx");
+        }
+        if (!loggedIn)
+        {
+            ErrorMessage.message = "Print Me!";
+            ErrorMessage.printMe = true;
+            Response.Redirect("HomePage.aspx");
+        }
+    }
+}
