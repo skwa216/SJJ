a31a84c50e7cdb2391e0a6c93b0b2a2bbfd70ef3
Jacob Homanics <jhh6@students.ptcollege.edu>
2017-03-08 15:19:02 -0500

    Add files via upload

---
+++ SJJ Site/SJJ-master/TheDVDExchangeWebsite/App_Code/SQLConnections.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for SQLConnections
+/// </summary>
+public class SQLConnections
+{
+    static SqlConnection dbConnection = new SqlConnection("Data Source=stusql;Initial Catalog=TheDVDExchangeDatabase ;Integrated Security=true");
+    public SqlCommand cmd = new SqlCommand();
+    public SqlDataReader reader;
+    public SQLConnections()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public void openDatabase()
+    {
+        //Opens database Connection
+        dbConnection.Open();
+       
+    }
+
+    public void closeDatabase()
+    {
+        //Closes dbConnection
+        dbConnection.Close();
+    }
+
+    public bool executeLoginCheckCommand(string usernameText, string passwordText)
+    {
+        cmd.Connection = dbConnection;
+        cmd.CommandText = "Select * from users where username = " + "'" + usernameText + "'";
+        reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            // Response.Write(reader["username"]);
+            // Response.Write(reader["userpassword"]);
+            // Response.Write(reader["useremail"]);
+            if (reader.GetValue(1).ToString() == usernameText && reader.GetValue(2).ToString() == passwordText)
+            {
+                ErrorMessage.message = "<p>You have been succesfully logged in!</p>";
+                ErrorMessage.printMe = true;
+                dbConnection.Close();
+                return true;
+            }
+            else
+            {
+                ErrorMessage.message = "<p>Username or password is incorrect</p>";
+                ErrorMessage.printMe = true;
+
+                dbConnection.Close();
+                return false;
+              
+
+            }
+
+        }
+
+        return false;
+    }
+
+ 
+}
---
+++ SJJ Site/SJJ-master/TheDVDExchangeWebsite/SignUpPage.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using Project;
+//2/23/2017
+//Jacob Homanics, Sam Kelsey, Jordyn DiSanzo
+//This class works in correlation with the SignUpPage.aspx webpage
+//Used to create a user and insert them into the database
+public partial class SignUpPage : System.Web.UI.Page
+{
+
+    public bool loggedIn;
+
+    //Initializes the string variables that will
+    //hold the values of the TextBoxes
+    public string usernametext;
+    public string passwordtext;
+    public string emailtext;
+    //Initializes an int to be used
+    //for how many rows are in the users database
+    public int count = 0;
+
+    //Initializes SQL Commands and Connection
+    public SQLConnections skwal = new SQLConnections();
+
+    public SqlCommand cmd = new SqlCommand();
+    SqlConnection dbConnection = new SqlConnection();
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        skwal.openDatabase();
+
+       
+        //Sets the dbConnection to the dbConnection Session
+      //  dbConnection = (SqlConnection)Session["DatabaseConnection"];
+    }
+
+    public void CreateAccount(object sender, EventArgs e)
+    {
+
+    
+        //Sets the string variables to the TextBox values
+        usernametext = usernameTextBox.Text;
+       
+            Response.Redirect("SignUpPage.aspx");
+       
+        passwordtext = passwordTextBox.Text;
+        emailtext = EmailTextBox.Text;
+        //Gets the count of the rows in the users table
+        cmd.CommandText = "Select count(*) from users";
+
+        //sets the cmd connection to dbConnection
+        cmd.Connection = dbConnection;
+        //opens the database
+        dbConnection.Open();
+
+        //Sets the int count to the number of rows in the users table
+        count = (int)cmd.ExecuteScalar();
+
+        //sets the command text to an insert statement
+        cmd.CommandText = "Insert into users (userid, username, userpassword, useremail) values ('" + (count + 1).ToString() + "', '" + usernametext +
+            "', '" + passwordtext + "', '" + emailtext + "')";
+
+        //Executes SQL Command 
+        cmd.ExecuteScalar();
+       //Closes database connection
+        dbConnection.Close();
+
+        Response.Write("Account created!");
+
+    }
+}
